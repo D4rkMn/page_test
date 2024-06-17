a3b6d8f70bef2d77768bd1e2b91780ee09098dd4
D4rkMn <d4rkmn0503@gmail.com>
2024-06-16 23:52:14 -0500

    initial page setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "export",
+	basePath: "/page_test",
+	images: {
+		unoptimized: true
+	}
+};
 
 export default nextConfig;
